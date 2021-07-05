ff461251e430b4b04767eacc18a217b75f552f65
JhonatanD09 <joranada@gmail.com>
2021-07-05 17:47:20 -0500

    v5.0

---
+++ delete.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+
+module.exports =  function deleteUser( nameUser ){
+   
+    const json = JSON.parse(fs.readFileSync(__dirname+"/names.json"))
+    let data = json["data"]
+
+    let listModified = {
+        "data":[]
+    }
+
+    let i = data.indexOf( nameUser );
+    if(i>= 0)
+        data.splice( i, 1 );
+        for (iterator in data){
+            listModified.data.push(data[iterator])
+        }
+        console.log(listModified)
+        fs.writeFileSync('names.json', JSON.stringify(listModified))
+        console.log(JSON.stringify(listModified))
+        console.log(json)
+}
--- manager.js
+++ manager.js
@@ -3,12 +3,18 @@ const app = express();
 const port = 3000;
 'use strict'
 const fs = require('fs')
+const deletUser = require(__dirname+ "/delete.js")
 
 app.get("/", (req, res) => {
   res.send(JSON.parse(fs.readFileSync(__dirname + '/names.json')));
   console.log()
-});
+})
 
+app.delete("/", (req,res)=>{
+  console.log("Hola")
+  deletUser(req.query.name)
+  res.send(JSON.parse(fs.readFileSync(__dirname+"/names.json")))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
