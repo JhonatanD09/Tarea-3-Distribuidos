2c93d7733dd2123870c24905761964a5465002bb
JhonatanD09 <joranada@gmail.com>
2021-07-05 14:46:19 -0500

    v 4.0

--- manager.js
+++ manager.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+'use strict'
+const fs = require('fs')
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(JSON.parse(fs.readFileSync(__dirname + '/names.json')));
+  console.log()
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
