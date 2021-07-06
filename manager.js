4796731e51e7fe494ddfde30fa7574e10045ed0e
DkSanjed <raulvalencia932@gmail.com>
2021-07-06 10:34:49 -0500

    manager V5

--- manager.js
+++ manager.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+'use strict'
 const fs = require('fs')
 const deletUser = require(__dirname+ "/delete.js")
 
+app.use('/', require('./get'))
+app.use("/",require("./post"))
 
 app.delete("/", (req,res)=>{
-  deletUser(req.query.name)w
+  deletUser(req.query.name)
   res.send(JSON.parse(fs.readFileSync(__dirname+"/names.json")))
 })
 
