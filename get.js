63fae802145a4b3ff67432c2c5780d81fead71dd
DkSanjed <raulvalencia932@gmail.com>
2021-07-06 10:31:16 -0500

    V10

--- get.js
+++ get.js
@@ -4,10 +4,9 @@ const router = express.Router();
 router.use(express.json());
 'use strict'
 const fs = require('fs')
-let json = JSON.parse(fs.readFileSync(__dirname + '/names.json'))
 
 router.get('/', (req, res)=>{
-    res.send(json);
+    res.send(JSON.parse(fs.readFileSync(__dirname + '/names.json')));
 })
 
 module.exports = router;
